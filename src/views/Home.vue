28958c59baa595d6e1ff1b2be7737ec0b93c4fbf
LU Yun <971779052@qq.com>
2025-11-11 20:33:09 +0800

    下载了Echarts，并且在Home.vue(id="card3")中添加了折线图(样式，数据都没有填充，所以没有显示)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,10 +13,11 @@
       卡片 2：“全城密接网络查看”，配 “查看完整网络” 按钮，点击直接跳转至 “密接人员展示” 页。
     </section>
 
-    <section class="card3">
+    <div id="card3">
       关键指标卡片（3-4 个）：用大数字直观展示，比如 “总密接次数”“涉及人员数”“次密接人数”“高密接区域数”。
-    </section>
+    </div>
     <section class="card4">
+
       关键指标卡片（3-4 个）：用大数字直观展示，比如 “总密接次数”“涉及人员数”“次密接人数”“高密接区域数”。
     </section>
     <section class="card5">
@@ -31,6 +32,112 @@
 </template>
 
 <script setup lang="ts" name="Home">
+
+
+import * as echarts from 'echarts';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  // 卡片三：折线图
+  var chartDom = document.getElementById('card3');
+  var myChart = echarts.init(chartDom);
+  var option;
+
+  option = {
+    title: {
+      text: '1000秒内感染的人数变化'
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          backgroundColor: '#6a7985'
+        }
+      }
+    },
+    legend: {
+      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: 'Email',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [120, 132, 101, 134, 90, 230, 210]
+      },
+      {
+        name: 'Union Ads',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [220, 182, 191, 234, 290, 330, 310]
+      },
+      {
+        name: 'Video Ads',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [150, 232, 201, 154, 190, 330, 410]
+      },
+      {
+        name: 'Direct',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [320, 332, 301, 334, 390, 330, 320]
+      },
+      {
+        name: 'Search Engine',
+        type: 'line',
+        stack: 'Total',
+        label: {
+          show: true,
+          position: 'top'
+        },
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [820, 932, 901, 934, 1290, 1330, 1320]
+      }
+    ]
+  };
+
+  option && myChart.setOption(option);
+})
+
+
 
 </script>
 
@@ -50,6 +157,6 @@
   /* padding: 20px; 增加内边距 */
   /* background-color: #f5f7fa; */
   /* 浅灰背景 */
-  color:black;
+  color: black;
 }
 </style>
